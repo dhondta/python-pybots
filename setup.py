23edfcccb81534bc0bee8e54a1b439042db1dff0
dhondta <alexandre.dhondt@gmail.com>
2018-01-04 19:53:58 +0100

    Fixed requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     "pybots.general",
     "pybots.specific",
   ],
-  version = "1.1.0",
+  version = "1.1.1",
   license = "AGPLv3",
   description = "A library for quickly creating client bots for communicating "
                 "with remote hosts.",
