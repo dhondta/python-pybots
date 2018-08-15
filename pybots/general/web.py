32030b9880a671c600d92dabecdd238e0e668986
dhondta <alexandre.dhondt@gmail.com>
2018-08-15 21:23:51 +0200

    Added random User-Agent support for WebBot

--- pybots/general/web.py
+++ pybots/general/web.py
@@ -19,6 +19,7 @@ import os
 import requests
 import urllib3
 from types import MethodType
+from user_agent import generate_user_agent
 try:  # Python3
     from urllib.request import urlretrieve
 except ImportError:  # Python2
@@ -50,8 +51,9 @@ class WebBot(Template):
     :param verbose:  debug level
     :param no_proxy: force ignoring the proxy
     """
-    headers = {'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64;"
-                             " rv:50.0) Gecko/20100101 Firefox/50.0",
+    headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                             "AppleWebKit/537.36 (KHTML, like Gecko) "
+                             "Chrome/60.0.3112.113 Safari/537.36",
                'Accept': "text/html,application/xhtml+xml,application/"
                          "xml;q=0.9,*/*;q=0.8",
                'Accept-Language': "en-US,en;q=0.5",
@@ -59,7 +61,9 @@ class WebBot(Template):
                'Connection': "keep-alive",
                'DNT': "1", 'Upgrade-Insecure-Requests': "1"}
 
-    def __init__(self, url, auth=None, verbose=False, no_proxy=False):
+    # TODO: handle public proxies ; see http://pgaref.com/blog/python-proxy/
+    def __init__(self, url, auth=None, verbose=False, no_proxy=False,
+                 random_uagent=False):
         super(WebBot, self).__init__(verbose, no_proxy)
         parsed = urlparse(url)
         if parsed.scheme == '' or parsed.netloc == '':
@@ -70,10 +74,14 @@ class WebBot(Template):
         if auth:
             self.session.auth = auth
         self.session.headers.update(self.headers)
+        # instantiating the bot with random_uagent will randomize the user agent
+        #  once and use it at the session level
+        if random_uagent:
+            self.session.headers.update({'User-Agent'; generate_user_agent()})
         self.session.proxies.update(self._proxies)
         self.logger.debug("Binding HTTP methods...")
         for m in SUPPORTED_HTTP_METHODS:
-            setattr(WebBot, m, MethodType(WebBot.template(m), self))
+            setattr(self, m, MethodType(WebBot.template(m), self))
 
     def __print_request(self):
         """
@@ -124,24 +132,35 @@ class WebBot(Template):
         :param aheaders: additional HTTP headers (dictionary)
         :post:           self.response, self.soup populated
         """
+        assert type(data or {}) is dict
+        assert type(aheaders or {}) is dict
         self.logger.debug("Requesting with method {}...".format(method))
         url = urljoin(self.url, rqpath)
         m = method.lower()
         if not hasattr(requests, m):
             self.logger.error("Bad request")
             raise AttributeError("requests has no method '{}'".format(m))
+        # handle user agent randomization ; this allows to randomize the user
+        #  agent at the request level and not the session one
+        if kw.pop('random_uagent', False):
+            aheaders = aheaders or {}
+            aheaders['User-Agent'] = generate_user_agent()
+        # now prepare the request
         request = requests.Request(method, url, data=data or {},
                                    headers=aheaders or {}, **kw)
         self._request = self.session.prepare_request(request)
         self.__print_request()
+        # handle change in proxies configuration and send the request
+        proxies = kw.pop('proxies', None)
         try:
             self.response = self.session.send(self._request,
-                                              allow_redirects=True)
+                allow_redirects=True, proxies=proxies or self.session.proxies)
         except:
             self.response = self.session.send(self._request,
-                                              allow_redirects=True,
-                                              verify=False)
+                allow_redirects=True, proxies=proxies or self.session.proxies,
+                verify=False)
         self.__print_response()
+        # handle HTTP status code here
         if 200 < self.response.status_code < 300:
             raise Exception("{} - {}".format(self.response.status_code,
                                              self.response.reason))
@@ -174,12 +193,12 @@ class WebBot(Template):
         """
         doc = """ Alias for request(method="{0}"). """
         if method == "get":
-            def f(self, rqpath="/", params=None, aheaders=None):
+            def f(self, rqpath="/", params=None, aheaders=None, **k):
                 return self.request(rqpath, method.upper(), None, aheaders,
-                                    params=params)
+                                    params=params, **k)
         if method in ["delete", "head", "options"]:
-            def f(self, rqpath="/", aheaders=None):
-                return self.request(rqpath, method.upper(), None, aheaders)
+            def f(self, rqpath="/", aheaders=None, **k):
+                return self.request(rqpath, method.upper(), None, aheaders, **k)
         elif method in ["post", "put"]:
             def f(self, rqpath="/", data=None, aheaders=None, **k):
                 return self.request(rqpath, method.upper(), data, aheaders, **k)
