bc9a1fc0d4ce7d945cdfeb93ca5f676bf01927fd
Alexandre D'Hondt <alexandre.dhondt@gmail.com>
2019-09-19 16:08:15 +0200

    Added __info__

---
+++ pybots/__info__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""Pybots package information.
+
+"""
+
+__author__    = "Alexandre D'Hondt"
+__version__   = "1.7.3"
+__copyright__ = "© 2019 A. D'Hondt"
+__license__   = "AGPLv3 (http://www.gnu.org/licenses/agpl.html)"
