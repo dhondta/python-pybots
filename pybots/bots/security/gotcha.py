745ee419e0ea297d26301b4edacdd567c36d4e73
dhondta <alexandre.dhondt@gmail.com>
2020-02-03 19:14:31 +0100

    Updated GotchaBot

--- pybots/bots/security/gotcha.py
+++ pybots/bots/security/gotcha.py
@@ -5,7 +5,7 @@
 from tinyscript.helpers.data.types import is_domain, is_email
 
 from ...core.protocols.http import HTTPBot
-from ...core.utils.api import cache
+from ...core.utils.api import *
 
 
 __all__ = ["GotchaBot"]
@@ -13,7 +13,7 @@ __all__ = ["GotchaBot"]
 URL = "https://gotcha.pw"
 
 
-class GotchaBot(HTTPBot):
+class GotchaBot(HTTPBot, API):
     """
     GotchaBot class for communicating with website Gotcha.
 
@@ -21,43 +21,28 @@ class GotchaBot(HTTPBot):
     :param kwargs:   HTTPBot keyword-arguments
     """
     def __init__(self, *args, **kwargs):
-        super(GotchaBot, self).__init__(URL, *args, **kwargs)
+        API.__init__(self, "", **kwargs)
+        HTTPBot.__init__(self, URL, *args, **kwargs)
     
+    @apicall
     @cache(300)
-    def _lookup(self, item):
+    def lookup(self, *items):
         """
-        Lookup for the given item.
+        Lookup for given items.
         
-        :param item: item to be searched (email or domain)
+        :param items: items to be searched (email or domain)
         """
-        if not is_email(item) and not is_domain(item) and \
-            not (item[0] == '@' and is_domain(item[1:])):
-            raise ValueError("Bad input (should be an email or a domain)")
-        if '*' in item:
-            raise ValueError("Wildcards are not allowed")
-        self.get("/search/{}".format(item))
-        content, rows = self.soup.find("div", attrs={'id': "content"}), []
-        if content is not None:
-            for row in content.find_all("div", attrs={'class': "row"}):
-                divs = row.find_all("div")
-                rows.append(list(map(lambda x: x.text, divs)))
-        return rows
-
-    def lookup(self, item):
-        """
-        Lookup for the given item.
-        
-        :param item: item to be searched (email or domain)
-        """
-        if not is_email(item) and not is_domain(item) and \
-            not (item[0] == '@' and is_domain(item[1:])):
-            raise ValueError("Bad input (should be an email or a domain)")
-        if '*' in item:
-            raise ValueError("Wildcards are not allowed")
-        self.get("/search/{}".format(item))
-        content, rows = self.soup.find("div", attrs={'id': "content"}), []
-        if content is not None:
-            for row in content.find_all("div", attrs={'class': "row"}):
-                divs = row.find_all("div")
-                rows.append(list(map(lambda x: x.text, divs)))
+        rows = []
+        for item in items:
+            if not is_email(item) and not is_domain(item) and \
+                not (item[0] == '@' and is_domain(item[1:])):
+                raise ValueError("Bad input (should be an email or a domain)")
+            if '*' in item:
+                raise ValueError("Wildcards are not allowed")
+            self.get("/search/{}".format(item))
+            content = self.soup.find("div", attrs={'id': "content"})
+            if content is not None:
+                for row in content.find_all("div", attrs={'class': "row"}):
+                    divs = row.find_all("div")
+                    rows.append(list(map(lambda x: x.text, divs)))
         return rows
