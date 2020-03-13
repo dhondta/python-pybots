aaec67382d57250e4043c61416335bef7f098991
dhondta <alexandre.dhondt@gmail.com>
2020-03-13 13:24:05 +0100

    Fixed mixins import

--- pybots/core/mixins/__init__.py
+++ pybots/core/mixins/__init__.py
@@ -3,7 +3,13 @@
 This package provides some additional features to be used as mixins in bots.
 
 """
-
 from .crawler import *
 from .downloader import *
 from .proxy import *
+
+from .crawler import __features__ as _crawler
+from .downloader import __features__ as _downloader
+from .proxy import __features__ as _proxy
+
+
+__all__ = __features__ = _crawler + _downloader + _proxy
