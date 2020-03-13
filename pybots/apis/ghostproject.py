15f1e5535d253ef0a7398ae1aebc9435b503b276
dhondta <alexandre.dhondt@gmail.com>
2020-03-13 17:16:41 +0100

    Fix apis.ghostproject

--- pybots/apis/ghostproject.py
+++ pybots/apis/ghostproject.py
@@ -2,8 +2,7 @@
 """Bot client dedicated to Nuclear Leaks.
 
 """
-import cfscrape
-import re
+import cloudscraper
 from tinyscript.helpers.data.types import *
 
 from ..core.utils.api import *
@@ -23,12 +22,16 @@ class GhostProjectAPI(API):
     def __init__(self, **kwargs):
         super(GhostProjectAPI, self).__init__(None, **kwargs)
         s = self._session.headers
-        #FIXME: get Cloudflare clearance cookie by following the protocol for
-        #        DDoS protection
-        s['Origin'] = self.url
         s['Content-Type'] = "application/x-www-form-urlencoded; charset=UTF-8"
         s['X-Requested-With'] = "XMLHttpRequest"
-     
+        try:
+            cookies, uagent = cloudscraper.get_tokens(self.url + "/x000x1337.php")
+        except Exception as e:
+            if "Could not collect tokens" in str(e):
+                raise APIError("Could not collect cloudflare clearance")
+        self._session.cookies.update(cookies)
+        self._session.headers['User-Agent'] = uagent
+
     def __validate(self, **kwargs):
         """
         Private generic validation function for API arguments.
@@ -45,15 +48,18 @@ class GhostProjectAPI(API):
         
         :param query: query string (email or domain with wildcard)
         """
-        super(GhostProjectAPI, self)._request("/x000x1337.php", "post",
-                                              data={'param': query})
+        super(GhostProjectAPI, self)._request("/x000x1337.php", "post", data={'param': query})
         if self._response.status_code != 200:
             raise APIError(self._response.reason)
-        lines = self._response.text.splitlines()
-        t, d = lines[0], filter(lambda x: x != "", lines[1:])
-        return {'time': t, 'data': {k.strip(): v.strip() for l in d \
-                                               for k, v in l.split(":")}}
-    
+        lines = self._response.text.strip("\r\n\"\\n").split("\\n")
+        t = float(lines[0].split("Time: ")[1].split("<")[0])
+        d = {}
+        for l in lines[1:]:
+            email, pswd = l.strip().split(":", 1)
+            d.setdefault(email, [])
+            d[email].append(pswd)
+        return {'time': t, 'error': d['Error'][0].strip()} if 'Error' in d.keys() else {'time': t, 'data': d}
+
     @apicall
     @cache(3600)
     def search_account(self, email):
