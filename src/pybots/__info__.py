9a9841fdf4496287466a09f321d93eac0631570c
dhondta <alexandre.dhondt@gmail.com>
2023-02-17 11:24:24 +0100

    Moved to pyproject.toml

---
+++ src/pybots/__info__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""Pybots package information.
+
+"""
+import os
+from datetime import datetime
+
+__y = str(datetime.now().year)
+__s = "2018"
+
+__author__    = "Alexandre D'Hondt"
+__copyright__ = "© {} A. D'Hondt".format([__y, __s + "-" + __y][__y != __s])
+__email__     = "alexandre.dhondt@gmail.com"
+__license__   = "GPLv3+ (https://www.gnu.org/licenses/gpl-3.0.fr.html)"
+__source__    = "https://github.com/dhondta/python-pybots"
+
+with open(os.path.join(os.path.dirname(__file__), "VERSION.txt")) as f:
+    __version__ = f.read().strip()
+
